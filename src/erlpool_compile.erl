bc3928fdf370ceacfa11cc83001f0cbcc1cd759a
anurag <anurag.gaurav@onextel.tech>
2024-10-14 14:21:29 +0530

    testing1

--- src/erlpool_compile.erl
+++ src/erlpool_compile.erl
@@ -1,31 +1,19 @@
 -module(erlpool_compile).
 
--define(GLOBALS_MODULE, erlpool_globals).
-
 -export([compile_settings/1]).
 
--include_lib("syntax_tools/include/merl.hrl").
+-define(SIZE_TABLE, erlpool_sizes).
 
 compile_settings(SettingsList) ->
-    Forms = get_settings_code(SettingsList),
-    %% for deubgging only
-    % file:write_file("/tmp/erlpool_globals_gen.erl", erl_prettypr:format(erl_syntax:form_list(Forms))),
-    case merl:compile_and_load(Forms) of
-        {ok, _Bin} ->
-            ok;
-        Err ->
-            Err
-    end.
-
-get_settings_code(SettingsList) ->
-    ModuleName = ?GLOBALS_MODULE,
-    Module = ?Q("-module('@ModuleName@')."),
-    Export = ?Q("-export([size/1])."),
-    Function = gen(SettingsList, []),
-    [Module, Export,  Function].
-
-gen([{Name, Size, _PoolArgs} | R], Acc) ->
-    gen(R, [?Q("(_@Name@) -> {ok, _@Size@}") | Acc]);
-gen([], Acc) ->
-    Clauses = lists:reverse([?Q("(_) -> {error, not_found}") | Acc]),
-    erl_syntax:function(merl:term(size), Clauses).
+    % Ensure the ETS table exists
+    case ets:info(?SIZE_TABLE) of
+        undefined ->
+            ets:new(?SIZE_TABLE, [named_table, public, {read_concurrency, true}]);
+        _ ->
+            ok
+    end,
+    % Update the ETS table with the new settings
+    lists:foreach(fun({Name, Size, _PoolArgs}) ->
+        ets:insert(?SIZE_TABLE, {Name, Size})
+    end, SettingsList),
+    ok.
